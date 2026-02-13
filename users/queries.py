11d01ecfa02a31d383c5045ede5653b31e30957e
joel magana <joelmagana15@gmail.com>
2026-02-13 07:22:13 +0300

    feat: database connection

--- users/queries.py
+++ users/queries.py
@@ -5,6 +5,5 @@ from feed.types import UserType
 class UserQuery(graphene.ObjectType):
     me = graphene.Field(UserType)
 
-    @login_required
     def resolve_me(self, info):
         return info.context.user
